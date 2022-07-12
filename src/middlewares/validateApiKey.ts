db6648f8ba9537498ba2155f058ff162d8d884ed
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-07-12 00:47:17 -0300

    feat: validates if employee already have a specific card type before creating a card

--- src/middlewares/validateApiKey.ts
+++ src/middlewares/validateApiKey.ts
@@ -5,7 +5,6 @@ import * as companyRepository from "../../repositories/companyRepository.js"
 export default async function validateApiKey(req: Request, res: Response, next: NextFunction) {
     const { authorization } = req.headers;
     const apiKey = authorization?.replace("X-API-Key:", "").trim();
-
     if (!apiKey) return res.status(401).send("No api-key sent");
 
     const company = await companyRepository.findByApiKey(apiKey);
--- src/middlewares/validateEmployee.ts
+++ src/middlewares/validateEmployee.ts
@@ -1,13 +1,19 @@
 import { Request, Response, NextFunction } from "express";
 
+import { TransactionTypes } from "../../repositories/cardRepository.js";
+
 import * as employeeRepository from '../../repositories/employeeRepository.js';
+import * as cardRepository from '../../repositories/cardRepository.js';
 
 export default async function validateEmployee(req: Request, res: Response, next: NextFunction) {
-    const { employeeId }: { employeeId: number } = req.body;
+    const { employeeId, type }: { employeeId: number, type: TransactionTypes } = req.body;
     const companyId = res.locals.company.id;
 
     const employee = await employeeRepository.findById(employeeId);
-    if (!employee || employee.companyId !== companyId) res.status(404).send("Employee not found");
+    if (!employee || employee.companyId !== companyId) return res.status(404).send("Employee not found");
+
+    const card = await cardRepository.findByTypeAndEmployeeId(type, employeeId);
+    if(card) return res.status(409).send(`Employee already have a/an ${type} card`);
 
     res.locals.employee = employee;
 
